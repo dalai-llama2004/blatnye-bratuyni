bbdd71697d6ef863ef957411d5e56297edb68fd9
joshi <g00gleisdork@gmail.com>
2025-11-21 20:13:22 +0300

    Revert "update all services"
    This reverts commit 8416ab7fbba30cd92743b62193813b5ba2dff237.

--- services/notification-service/main.py
+++
@@ -1,5 +0,0 @@
-from fastapi import FastAPI
-from .routes import router
-
-app = FastAPI(title="Notification Service")
-app.include_router(router)
--- services/notification-service/models.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, EmailStr
-
-class EmailNotification(BaseModel):
-    email: EmailStr
-    subject: str
-    text: str

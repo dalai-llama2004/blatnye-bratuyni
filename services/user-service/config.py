640a80816e9442e44881258b0ba7c0305ebaad84
Ilya Osipov <adioshuesos@gmail.com>
2025-11-21 17:11:27 +0300

    feat: add password hashing and JWT generation

--- services/user-service/config.py
+++ services/user-service/config.py
@@ -5,6 +5,11 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
+# Параметры DB
 DB_URL = os.getenv("DB_URL", "postgresql://postgres:postgres@localhost:5432/userservice")
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# JWT
+SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
